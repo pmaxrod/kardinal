e44dc5748bedcabd6f3996354ec3ece288d0ddca
Pablo Maxiá <pmaxiarodriguez@gmail.com>
2026-03-01 19:25:28 +0100

    Primer commit

---
+++ app/app/settings/dev.py
@@ -0,0 +1,25 @@
+from .base import *
+import os
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
+DEBUG = os.environ.get("DEBUG")
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+
+# Database PostgreSQL
+DATABASES = {
+    'default': {
+        'ENGINE': os.environ.get("SQL_ENGINE"),
+        'NAME': os.environ.get("SQL_DATABASE_NAME"),
+        'USER': os.environ.get("SQL_USER"),
+        'PASSWORD': os.environ.get("SQL_PASSWORD"),
+        'HOST': os.environ.get("SQL_HOST"),
+        'PORT': os.environ.get("SQL_PORT"),
+    }
+}
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+try:
+    from .local import *
+except ImportError:
+    pass
---
+++ app/home/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+from wagtail.models import Page
+from wagtail.fields import RichTextField
+
+# import MultiFieldPanel:
+from wagtail.admin.panels import FieldPanel, MultiFieldPanel
+
+class HomePage(Page):
+    image = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+",
+        help_text="Imagen de inicio",
+    )
+
+    hero_text = models.CharField(
+        blank=True,
+        max_length=255, help_text="Introducción del sitio"
+    )
+
+    body = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        MultiFieldPanel(
+            [
+                FieldPanel("image"),
+                FieldPanel("hero_text")
+            ],
+            heading="Sección",
+        ),
+        FieldPanel('body'),
+    ]
