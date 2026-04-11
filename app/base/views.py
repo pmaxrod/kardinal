89c0a22c7d4643064660f6b49b1ad4a82547c7fc
Pablo Maxiá <pmaxiarodriguez@gmail.com>
2026-04-11 20:43:23 +0200

    Eliminar widget ThemeOption

--- app/base/views.py
+++ app/base/views.py
@@ -1,20 +1,19 @@
 from django.shortcuts import redirect
 from django.views import View
-from base.models import ThemeOption
-
+from users.models import UserSettings
 class ToggleThemeView(View):
     def post(self, request, *args, **kwargs):
         theme = request.POST.get('theme')
 #        themes = ["system", "light", "dark"]
-        themes = ThemeOption.objects.values_list("value", flat=True)
-        if theme in themes:
-            request.session["theme"] = theme
+        #themes = ThemeOption.objects.values_list("value", flat=True)
+        #if theme in themes:
+        request.session["settings"]["theme"] = theme
         #return redirect(request.META.get("HTTP_REFERER", "users.profile"))
         return redirect(request.META.get("HTTP_REFERER"))
 
 class ChangeFontView(View):
     def post(self, request, *args, **kwargs):
         font = request.POST.get('font')
-        if font in ['font-sans', 'font-serif']:
-            request.session["font"] = font
+        #if font in ['font-sans', 'font-serif']:
+        request.session["settings"]["font"] = font
         return redirect(request.META.get("HTTP_REFERER"))
