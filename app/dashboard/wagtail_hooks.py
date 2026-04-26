af5df1e6ca19a9c2662427b706bcd0cb8457ac27
Pablo Maxiá <pmaxiarodriguez@gmail.com>
2026-04-26 21:10:57 +0200

    Personalizar interfaz de administrador

--- app/dashboard/wagtail_hooks.py
+++ app/dashboard/wagtail_hooks.py
@@ -1,6 +1,11 @@
 from wagtail import hooks
-
+from django.templatetags.static import static
+from django.utils.html import format_html
 
 @hooks.register("get_avatar_url")
-def get_profile_picture(user, size):
+def get_profile_avatar(user, size):
     return user.profile_picture
+
+@hooks.register("insert_global_admin_css")
+def global_admin_css():
+    return format_html(f'<link rel="stylesheet" href={static("css/admin.css")}>')
