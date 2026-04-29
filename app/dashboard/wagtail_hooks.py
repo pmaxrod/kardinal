50334c42eed850a0fa2056f5f425112fd4b08ed8
Pablo Maxiá <pmaxiarodriguez@gmail.com>
2026-04-29 20:56:39 +0200

    Añadir iconos a la interfaz de administración de Wagtail

--- app/dashboard/wagtail_hooks.py
+++ app/dashboard/wagtail_hooks.py
@@ -1,16 +1,15 @@
-from django.urls import path
 from wagtail import hooks
 from django.templatetags.static import static
 from django.utils.html import format_html
-
-from dashboard.views import CustomAccountView
-
-
-@hooks.register("get_avatar_url")
-def get_profile_avatar(user, size):
-    return user.profile_picture
 
 
 @hooks.register("insert_global_admin_css")
 def global_admin_css():
     return format_html(f'<link rel="stylesheet" href={static("css/admin.css")}>')
+
+
+@hooks.register("register_icons")
+def register_icons(icons):
+    icons.append("wagtailadmin/icons/blogindex.svg")
+    icons.append("wagtailadmin/icons/blogpost.svg")
+    return icons
