5b74e5ca96c29111716c3733625de47de0c32d97
Pablo Maxiá <pmaxiarodriguez@gmail.com>
2026-05-01 20:10:03 +0200

    Añadir 'Me gusta' a comentarios

--- app/comments/urls.py
+++ app/comments/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from comments import views
 
 urlpatterns = [
-    path("page/<str:slug>/add/", views.add_page_comment, name="add_comment"),
+    path("add/<int:pk>/", views.add_page_comment, name="add_comment"),
     path("edit/<int:pk>/", views.edit_comment, name="edit_comment"),
-    path("delete/<int:pk>/",views.delete_comment, name="delete_comment")
+    path("delete/<int:pk>/",views.delete_comment, name="delete_comment"),
+    path("like/<int:pk>/", views.like_comment, name="like_comment")
 ]
--- app/comments/views.py
+++ app/comments/views.py
@@ -1,39 +1,40 @@
 # Create your views here.
+from django.utils.translation import gettext as _
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
 from django.views.decorators.http import require_http_methods
-from wagtail.models import Page
+from blog.models import BlogPostPage
 from comments.forms import CommentForm
-from comments.models import Comment
+from comments.models import Comment, CommentLike
+
 
 @login_required
-@require_http_methods(['POST'])
-def add_page_comment(request, slug):
+@require_http_methods(["POST"])
+def add_page_comment(request, pk):
     """Añade un comentario a la página.
 
     Arguments:
         request -- Petición realizada a la página web
-        slug -- Slug de la página que se comenta
 
     Returns:
         Respuesta HTTP con el comentario añadido
     """
-    form = CommentForm(request.POST)
-    page = Page.objects.filter(slug=slug).first().specific
-    form.initial['user'] = request.user
-    form.initial['page'] = page
+    # page = Page.objects.filter(slug=slug).first().specific
+    page = BlogPostPage.objects.get(pk=pk)
+    form = CommentForm(request.POST, initial={"user": request.user, "page": page})
     if form.is_valid():
         comment = form.save(commit=False)
-        comment.user = request.user
-        comment.page = page
+        comment.user = form.initial.get("user")
+        comment.page = form.initial.get("page")
         comment.save()
-        context = {'comment': comment}
+        context = {"comment": comment}
         return render(request, "partials/comment.html", context)
 
+
 @login_required
-@require_http_methods(['GET', 'POST'])
+@require_http_methods(["GET", "POST"])
 def edit_comment(request, pk):
     """Edita un comentario.
 
@@ -46,21 +47,22 @@ def edit_comment(request, pk):
     """
     comment = get_object_or_404(Comment, pk=pk, user=request.user)
     form = CommentForm(instance=comment)
-    context = {'form': form, 'comment': comment}
+    context = {"form": form, "comment": comment}
     if request.method == "POST":
         form = CommentForm(request.POST, instance=comment)
         if form.is_valid():
             form.save()
-            context = {'comment': comment, 'form': form}
+            context = {"comment": comment, "form": form}
             messages.success(request, "El comentario se ha podido editar con éxito.")
             return render(request, "partials/comment.html", context)
         else:
             messages.error(request, "El comentario no se ha podido editar.")
     elif request.method == "GET":
         return render(request, "partials/edit_comment_form.html", context)
-    
+
+
 @login_required
-@require_http_methods(['GET', 'DELETE'])    
+@require_http_methods(["GET", "DELETE"])
 def delete_comment(request, pk):
     """Borra un comentario.
 
@@ -75,8 +77,25 @@ def delete_comment(request, pk):
     if request.method == "DELETE":
         comment.delete()
         response = HttpResponse(status=204)
-        response['HX-Trigger'] = 'delete-comment'
+        response["HX-Trigger"] = "delete-comment"
         messages.success(request, "El comentario se ha borrado con éxito.")
         return response
     elif request.method == "GET":
-        return render(request, "partials/delete_comment_form.html", {'comment': comment})
+        return render(
+            request, "partials/delete_comment_form.html", {"comment": comment}
+        )
+
+
+@login_required
+@require_http_methods(["POST"])
+def like_comment(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+
+    comment_like = CommentLike.objects.filter(comment=comment, user=request.user)
+    if comment_like.exists():
+        comment_like.delete()
+    else:
+        comment_like.create(comment=comment, user=request.user)
+
+    context = {"comment": comment}
+    return render(request, "partials/comment_likes.html", context)
