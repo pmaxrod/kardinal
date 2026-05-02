bc27f6c18bc43a480848529888c04d2ef5959900
Pablo Maxiá <pmaxiarodriguez@gmail.com>
2026-05-02 21:06:05 +0200

    Personalizar las opciones de la interfaz de administración según rol de usuario

--- app/dashboard/wagtail_hooks.py
+++ app/dashboard/wagtail_hooks.py
@@ -5,11 +5,20 @@ from django.utils.html import format_html
 
 @hooks.register("insert_global_admin_css")
 def global_admin_css():
+    """Inserta CSS para las páginas de administración."""
     return format_html(f'<link rel="stylesheet" href={static("css/admin.css")}>')
 
 
 @hooks.register("register_icons")
 def register_icons(icons):
+    """Registra iconos para las páginas de administración."""
     icons.append("wagtailadmin/icons/blogindex.svg")
     icons.append("wagtailadmin/icons/blogpost.svg")
     return icons
+
+@hooks.register("construct_main_menu")
+def hide_menu_options(request, menu_items):
+    """Oculta opciones solo para el administrador a los bloggers."""
+    admin_items = ["explorer", "reports", "settings"]
+    if request.user.groups.filter(name__in=["Bloggers"]):
+        menu_items[:] = [item for item in menu_items if item.name not in admin_items]
