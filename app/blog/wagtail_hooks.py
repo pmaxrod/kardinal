c88d5d7be54e2eff121f8aae1144cff021caaa47
Pablo Maxiá <pmaxiarodriguez@gmail.com>
2026-05-02 21:06:28 +0200

    Bloquear acciones peligrosas sobre las páginas mediante hooks

--- app/blog/wagtail_hooks.py
+++ app/blog/wagtail_hooks.py
@@ -1,6 +1,9 @@
+from django.contrib import messages
+from django.http import HttpResponseRedirect
 from django.utils.translation import gettext as _
 from wagtail import hooks
 from wagtail.admin.ui.tables import Column, UserColumn, RelatedObjectsColumn
+from wagtail.admin.views.pages.listing import IndexView
 from wagtail.admin.viewsets.pages import PageListingViewSet
 from wagtail.admin.viewsets.base import ViewSetGroup
 from wagtail.snippets.views.snippets import SnippetViewSet, SnippetViewSetGroup
@@ -8,12 +11,47 @@ from blog.models import BlogCategory, BlogIndexPage, BlogPostPage, Tag
 
 
 # Viewsets de páginas
+class BlogIndexView(IndexView):
+    """Vista para los índices de blogs."""
+    model = BlogIndexPage
+
+    def get_base_queryset(self):
+        user = self.request.user
+        if user.is_superuser:
+            return super().get_base_queryset()
+        else:
+            pages = (
+                self.model.objects.filter(depth__gt=1)
+                .filter(owner=self.request.user)
+                .values_list("pk", flat=True)
+            )
+            pages = self.annotate_queryset(pages)
+            return pages
+
+class BlogPostView(IndexView):
+    """Vista para las entradas de blog."""
+    model = BlogPostPage
+
+    def get_base_queryset(self):
+        user = self.request.user
+        if user.is_superuser:
+            return super().get_base_queryset()
+        else:
+            pages = (
+                self.model.objects.filter(depth__gt=1)
+                .filter(owner=self.request.user)
+                .values_list("pk", flat=True)
+            )
+            pages = self.annotate_queryset(pages)
+            return pages
+        
 class BlogIndexListingViewSet(PageListingViewSet):
     """Listado de blogs de usuario."""
 
     model = BlogIndexPage
-    menu_label = _("Blogs de usuarios")
+    menu_label = _("Índice del blog")
     icon = "thumbtack"
+    index_view_class = BlogPostView
 
     columns = PageListingViewSet.columns + [
         UserColumn("owner", label=_("Propietario"), sort_key="owner"),
@@ -25,7 +63,7 @@ class BlogPostListingViewSet(PageListingViewSet):
     """Listado de entradas de blog."""
 
     model = BlogPostPage
-    menu_label = _("Entradas de blog")
+    menu_label = _("Entradas del blog")
     icon = "blogpost"
 
     columns = PageListingViewSet.columns + [
@@ -40,8 +78,9 @@ class BlogPostListingViewSet(PageListingViewSet):
 class BlogPagesViewSetGroup(ViewSetGroup):
     """Agrupador de páginas asociadas a blogs."""
 
-    menu_label = _("Páginas de blogs")
+    menu_label = _("Blog")
     menu_icon = "blogindex"
+    menu_order = 200
     items = (BlogIndexListingViewSet("blogs"), BlogPostListingViewSet("blogposts"))
 
 
@@ -86,3 +125,40 @@ def register_blog_viewset():
 @hooks.register("register_admin_viewset")
 def register_blog_snippets_viewset():
     return BlogSnippetsViewSetGroup()
+
+
+# Hooks para evitar comportamiento irresponsable de usuarios
+protected_pages = ("homepage", "blogdashboardpage", "blogindexpage")
+
+
+@hooks.register("before_create_page")
+def disable_extra_blogs(request, parent_page, page_class):
+    """Impide que los usuarios puedan tener más de un blog."""
+    if page_class == BlogIndexPage:
+        has_blog = page_class.objects.filter(owner=request.user).exists()
+        if has_blog:
+            messages.error(request, "No se puede tener más de un blog.")
+            return HttpResponseRedirect(request.headers["referer"])
+
+
+@hooks.register("before_copy_page")
+def disable_copy_page(request, page):
+    """Bloquea el copiado de páginas."""
+    messages.error(request, "No se pueden copiar páginas.")
+    return HttpResponseRedirect(request.headers["referer"])
+
+
+@hooks.register("before_unpublish_page")
+def disable_unpublish_pages(request, page):
+    """Impide que se puedan despublicar páginas importantes de la aplicación."""
+    if page.content_type.model in protected_pages:
+        messages.error(request, f"No se puede despublicar: '{page}'.")
+        return HttpResponseRedirect(request.headers["referer"])
+
+
+@hooks.register("before_delete_page")
+def disable_delete_pages(request, page):
+    """Impide que se puedan borrar páginas importantes de la aplicación."""
+    if page.content_type.model in protected_pages:
+        messages.error(request, f"No se puede borrar '{page}'.")
+        return HttpResponseRedirect(request.headers["referer"])
